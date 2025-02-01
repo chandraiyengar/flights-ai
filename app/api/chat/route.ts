23800bf731e6c9e9714f022f4be7cbc63032bc7a
Chandrasekhar Iyengar <chandrasekhariyengar@gmail.com>
2025-02-01 23:24:49 +0000

    add google knowledge base search in

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -33,7 +33,8 @@ export async function POST(req: Request) {
     'give them a summary of the flights, as one piece of text.' +
     'For each flight, you should say the time it is leaving and the price.' +
     'If there are more than 4 flights, just say the range of prices and times.' +
-    'Do not give a list of more than 4 flights!',
+    'Do not give a list of more than 4 flights!' +
+    'If there are flights from different airports, make sure to mention which airports they are from.',
     messages,
     tools: {
       weather: tool({
@@ -53,6 +54,38 @@ export async function POST(req: Request) {
         description: 'Search for available flights between airports',
         parameters: SearchFlightsParams,
         execute: (searchFlightParams) => searchFlights(searchFlightParams),
+      }),
+      findPlaceCode: tool({
+        description: 'Find the place code for a location which has more than one airport.',
+        parameters: z.object({
+          location: z.string().describe('The location to find the place code for'),
+        }),
+        execute: async ({ location }) => {
+          const params = new URLSearchParams({
+            query: location,
+            key: process.env.GOOGLE_API_KEY || '',
+            limit: '1',
+            types: 'Place'
+          });
+
+          const response = await fetch(
+            `https://kgsearch.googleapis.com/v1/entities:search?${params}`
+          );
+
+          if (!response.ok) {
+            throw new Error('Failed to fetch place data');
+          }
+
+          const data = await response.json();
+          const result = data.itemListElement[0]?.result;
+
+          if (!result) {
+            throw new Error('No results found');
+          }
+          return {
+            placeCode: result['@id'].slice(3),
+          };
+        },
       }),
     },
     maxSteps: 10,
